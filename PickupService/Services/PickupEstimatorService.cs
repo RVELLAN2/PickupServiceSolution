895772aed84b430ea2fccdcd798f0759d492a832
RVELLAN2 <52933708+RVELLAN2@users.noreply.github.com>
2020-11-11 13:44:49 -0500

    Add project files.

---
+++ PickupService/Services/PickupEstimatorService.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PickupService.Services
+{
+    public class PickupEstimatorService : PickupEstimator.PickupEstimatorBase
+    {
+        public override Task<PickupResponse> GetPickupTime(PickupRequest request, ServerCallContext context)
+        {
+            // for orders under 5 items, tomorrow
+            // over 5 items, 2 days
+            int days = 0;
+            if(request.Items.Count <= 5)
+            {
+                days = 1;
+            }
+            else
+            {
+                days = 2;
+            }
+
+            var response = new PickupResponse
+            {
+                PickupTime = Timestamp.FromDateTime(DateTime.Now.AddDays(days).ToUniversalTime())
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
